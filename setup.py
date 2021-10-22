04d144333cb016d682cf494e8a0cde72b243b346
Hunter Wilhelm <zicy@zicy.net>
2021-10-22 11:10:52 -0600

    updated git url

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='cse251',
     version='0.0.1',
     description='The package that contains common classes for the CSE 251 course',
-    # url='',
+    url='https://github.com/yeskindofday/cse251-library.git',
     author='Brother Comeau',
     license='MIT',
     packages=['cse251'],
