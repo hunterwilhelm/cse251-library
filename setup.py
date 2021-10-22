8fffd5b77ef307c949933d164560d8ca7905c0e8
Hunter Wilhelm <zicy@zicy.net>
2021-10-22 11:10:14 -0600

    ready for publish

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='cse251',
+    version='0.0.1',
+    description='The package that contains common classes for the CSE 251 course',
+    # url='',
+    author='Brother Comeau',
+    license='MIT',
+    packages=['cse251'],
+    install_requires=['matplotlib', 'numpy'],
+    zip_safe=False
+)
