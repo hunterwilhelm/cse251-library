3c23b200856a3d277345709091e22d16a7fb1c27
Hunter Wilhelm <zicy@zicy.net>
2021-11-04 23:23:33 -0600

    updated username

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='cse251',
     version='0.0.1',
     description='The package that contains common classes for the CSE 251 course',
-    url='https://github.com/yeskindofday/cse251-library.git',
+    url='https://github.com/hunterwilhelm/cse251-library',
     author='Brother Comeau and Hunter Wilhelm',
     license='none',
     packages=['cse251'],
